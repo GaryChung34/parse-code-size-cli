252ba52acdb140c0829cc354162c4f351ef4ef02
Gary Chung <gary64800@gmail.com>
2022-03-24 20:05:25 +0800

    add ShowFile and on progress on inject dependency

--- src/main/java/com/example/parse/ParseCommand.java
+++ src/main/java/com/example/parse/ParseCommand.java
@@ -1,8 +1,11 @@
 package com.example.parse;
 
+import jakarta.inject.Inject;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+
+import java.io.File;
 
 @Command(name = "parse", description = "parse source code file in each directory.",
     mixinStandardHelpOptions = true)
@@ -14,9 +17,26 @@ public class ParseCommand implements Runnable{
     @Option(names = {"--here"}, description = "parse in current position")
         Boolean here;
 
+    @Inject
+    ShowFile showFile = new ShowFile();
+
     @Override
     public void run() {
         System.out.println("parse command is running...");
+
+        String fname = "C:\\Users\\Gary-Home\\repos\\parse-code-size-cli\\file-io-java";
+
+        File file = new File(fname);
+        showFile.countSubFiles(file);
+        System.out.println("max: " + showFile.maxLines);
+
+        File[] files = file.listFiles();
+
+        System.out.println(file.getName() + " :");
+
+        for (int i = 0; i < files.length; i++) {
+            showFile.showSubFiles(files[i]);
+        }
 
     }
 }
--- src/main/java/com/example/parse/ShowFile.java
+++ src/main/java/com/example/parse/ShowFile.java
@@ -1,16 +1,18 @@
+package com.example.parse;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 
-public class Main {
+public class ShowFile {
     long maxLines = 0;
 
     public static long countLine(File file) {
         long lines = 0;
 
         try (LineNumberReader reader = new LineNumberReader(new FileReader(file))) {
-            
+
             while (reader.readLine() != null);
             lines = reader.getLineNumber();
 
@@ -22,7 +24,7 @@ public class Main {
     }
 
     public void showSubFiles(File file) {
-         String fname = "C:\\Users\\Gary-Home\\repos\\parse-code-size-cli\\file-io-java";
+        String fname = "C:\\Users\\Gary-Home\\repos\\parse-code-size-cli\\file-io-java";
 //        String fname = "C:\\Users\\Gary-Home\\IdeaProjects\\eformss-backend\\src\\main\\java\\gov\\edb\\eformss";
 
         if (file.isDirectory()) {
@@ -61,48 +63,10 @@ public class Main {
 
         long weight = countLine(file);
         long result = Math.round((double)weight/maxLines*10);
-        
+
         // System.out.println("his: " + result);
         for (int i = 0; i < result; i++) {
             System.out.print("#");
         }
-    }
-
-    // public static long countMaxLines(File file) {
-    //     long result = 0;
-
-    //     if (file.isDirectory()) {
-    //         File[] files = file.listFiles();
-
-    //         for (int i = 0; i < files.length; i++){
-                
-    //         }
-
-    //     } else {
-    //         countLine
-    //     }
-
-    //     return result;
-    // }
-
-    public static void main(String[] args) {
-        Main main = new Main();
-         String fname = "C:\\Users\\Gary-Home\\repos\\parse-code-size-cli\\file-io-java";
-//        String fname = "C:\\Users\\Gary-Home\\IdeaProjects\\eformss-backend\\src\\main\\java\\gov\\edb\\eformss";
-        File file = new File(fname);
-        main.countSubFiles(file);
-        System.out.println("max: " + main.maxLines);
-
-        File[] files = file.listFiles();
-
-        System.out.println(file.getName() + " :");
-
-        for (int i = 0; i < files.length; i++) {
-            main.showSubFiles(files[i]);
-        }
-
-        // System.out.println();
-        // File testFile = new File("C:\\Users\\user\\repos\\file-io-java\\Main.java");
-        // System.out.println(testFile.getName() + ": " + countLine(testFile));
     }
 }
